b3c3cf97838ae69b69e610cc380e6705833a6b6d
e <e@e.ee>
2025-03-29 16:10:20 +0200

    Version bump to 0.7.250329+11: Make help-finger show help

--- lib/globals.dart
+++ lib/globals.dart
@@ -12,7 +12,7 @@ final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey = GlobalKey<Scaffol
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
-const String progVersion = '0.6.250316';
+const String progVersion = '0.7.250329';
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
