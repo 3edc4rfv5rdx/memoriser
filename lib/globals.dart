01a113b897121521710806099fef63ad4351bf49
e <e@e.ee>
2025-04-06 15:33:37 +0300

    Version bump to 0.8.250406+18: null strings, filters etc

--- lib/globals.dart
+++ lib/globals.dart
@@ -15,8 +15,8 @@ final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
-const String progVersion = '0.8.250403';
-const int buildNumber = 17;
+const String progVersion = '0.8.250406';
+const int buildNumber = 18;
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
