c55adc1215e0e1b5d9f6a281aebd6c2e9c408228
e <e@e.ee>
2025-05-23 13:32:47 +0300

    Version bump to 0.8.250523+39: todays red events

--- lib/globals.dart
+++ lib/globals.dart
@@ -16,7 +16,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 const String progVersion = '0.8.250523';
-const int buildNumber = 38;
+const int buildNumber = 39;
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
