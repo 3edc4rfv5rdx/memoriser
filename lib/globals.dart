0418ab4832292d2444ab56ed1ca815063dbb4070
e <e@e.ee>
2025-05-30 11:57:30 +0300

    Version bump to 0.9.250530+45: fix reminders after restore

--- lib/globals.dart
+++ lib/globals.dart
@@ -15,8 +15,8 @@ final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
-const String progVersion = '0.9.250523';
-const int buildNumber = 44;
+const String progVersion = '0.9.250530';
+const int buildNumber = 45;
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
