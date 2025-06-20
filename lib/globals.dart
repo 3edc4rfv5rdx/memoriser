91749334b9ad6d233b59121e57c3f76a3a85b44d
e <e@e.ee>
2025-06-20 14:43:56 +0300

    Version bump to 0.9.250620+59: add Swipe to edit/delete to main list

--- lib/globals.dart
+++ lib/globals.dart
@@ -16,7 +16,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 const String progVersion = '0.9.250620';
-const int buildNumber = 58;
+const int buildNumber = 59;
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
