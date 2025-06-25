ad593a40a54c186b4d5f00a2611979c84702718f
e <e@e.ee>
2025-06-25 15:13:20 +0300

    Version bump to 0.9.250625+61: Add virtual folder Yearly

--- lib/globals.dart
+++ lib/globals.dart
@@ -15,8 +15,8 @@ final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
-const String progVersion = '0.9.250624';
-const int buildNumber = 60;
+const String progVersion = '0.9.250625';
+const int buildNumber = 61;
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
