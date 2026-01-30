fba0d477999636399690e55ae96d234538be5153
e <e@e.ee>
2026-01-30 08:26:41 +0200

    Version bump to 0.9.260130+75: add full/lock screen

--- lib/globals.dart
+++ lib/globals.dart
@@ -15,8 +15,8 @@ final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
-const String progVersion = '0.9.260129';
-const int buildNumber = 74;
+const String progVersion = '0.9.260130';
+const int buildNumber = 75;
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
