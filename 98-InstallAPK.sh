de3cd771009ba184c61719970c4f90809527a7e9
e <e@e.ee>
2026-02-10 12:09:46 +0200

    new script update

--- 98-InstallAPK.sh
+++ 98-InstallAPK.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-dst="${1%x}"
 
-cp "$1" "$dst"
-
-echo $dst
+src=$(ls -t Mem* | head -n1)
+dst="${src%x}"
+cp "$src" "$dst"
+echo "$dst"
 adb -s RFCW91FV79X install -r $dst
-
 rm -f "$dst"
