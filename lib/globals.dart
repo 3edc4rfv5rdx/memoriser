b603adbf6fad93d833f211bc907644fd23fe0796
e <e@e.ee>
2026-03-03 16:49:25 +0200

    Version bump to 0.9.260303+122: alarms errors, fixes

--- lib/globals.dart
+++ lib/globals.dart
@@ -16,7 +16,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 const String progVersion = '0.9.260303';
-const int buildNumber = 121;
+const int buildNumber = 122;
 const String progAuthor = 'Eugen';
 
 const String localesFile = 'assets/locales.json';
